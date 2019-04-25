4452c6bd366091a628d18466b34206e2be715ed1
seysay <master.sey7@gmail.com>
2019-04-25 15:25:43 +0300

    clean space

--- web/index.php
+++ web/index.php
@@ -1,13 +1,10 @@
 <?php
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../app/Core/Config.php';
 
-    $router = new \Bramus\Router\Router();
+$router = new \Bramus\Router\Router();
 
-    require_once "../app/Core/Router.php";
+require_once "../app/Core/Router.php";
 
-    $router->run();
+$router->run();
